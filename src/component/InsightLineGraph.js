d2f1760dc322aafc86051b5bc358484c2f6a4ad1
Wajhul Huda <62687753+Wajhulhuda@users.noreply.github.com>
2023-04-30 10:23:19 +0530

    Add files via upload
    completed

---
+++ src/component/InsightLineGraph.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+const InsightLineGraph = ({ graphData }) => {
+  // eslint-disable-next-line
+  const [state, setState] = useState({
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+    ],
+  });
+  return (
+    <div className="col-lg-12">
+      <Chart options={state.options} series={state.series} type="line" />
+    </div>
+  );
+};
+
+export default InsightLineGraph;
